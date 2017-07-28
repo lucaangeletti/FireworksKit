d92bf59291f313c043742906686445d85e45d483
“luca_angeletti“ <lucaangeletti@me.com>
2017-07-28 19:54:20 +0200

    Initial commit

---
+++ FireworksKit/Effect.swift
@@ -0,0 +1,13 @@
+//
+//  Effect.swift
+//  FireworksKit
+//
+//  Created by Luca Angeletti on 28/07/2017.
+//  Copyright © 2017 Luca Angeletti. All rights reserved.
+//
+
+import Foundation
+
+public enum Effect {
+
+}
---
+++ FireworksKit/FireworksKit.h
@@ -0,0 +1,19 @@
+//
+//  FireworksKit.h
+//  FireworksKit
+//
+//  Created by Luca Angeletti on 28/07/2017.
+//  Copyright © 2017 Luca Angeletti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FireworksKit.
+FOUNDATION_EXPORT double FireworksKitVersionNumber;
+
+//! Project version string for FireworksKit.
+FOUNDATION_EXPORT const unsigned char FireworksKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FireworksKit/PublicHeader.h>
+
+
